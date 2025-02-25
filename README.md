e691a52376a6c3a271d87c3cf80797b4927c1052
georgexian <1015729521@qq.com>
2025-02-25 16:12:04 +0800

    12

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LogRecord
+
+
+LogRecord.get().init(this).setFolder("dmr")
+.setInformat("yyyy-MM-dd")
+.setNotifyDesc("日志记录服务")
+.setNotifyId(12)
+.setNotifyIcon(R.mipmap.ic_launcher)
+.setOffDesc("关闭日志")
+.setOnDesc("开始日志")
+.setControlBroadcastAction("dmr_log_start","dmr_log_stop")
+.setFolder("ALOG")
+.setPrefix("DMR")
--- app/src/main/java/com/georgel/logrecord/MainActivity.kt
+++ app/src/main/java/com/georgel/logrecord/MainActivity.kt
@@ -26,6 +26,17 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        LogRecord.get().init(this).setFolder("dmr")
+            .setInformat("yyyy-MM-dd")
+            .setNotifyDesc("日志记录服务")
+            .setNotifyId(12)
+            .setNotifyIcon(R.mipmap.ic_launcher)
+            .setOffDesc("关闭日志")
+            .setOnDesc("开始日志")
+            .setControlBroadcastAction("dmr_log_start","dmr_log_stop")
+            .setFolder("ALOG")
+            .setPrefix("DMR")
     }
 }
 
